435080f5c610a1edcf96c0187d208666b0dd52eb
Axel Rothe <me@axelrothe.com>
2022-06-28 19:57:05 +0200

    query fixes

--- src/ninox.cjs
+++ src/ninox.cjs
@@ -303,7 +303,7 @@ class Ninox {
 
 		const result = await axios.post(
 			`https://api.ninox.com/v1/teams/${this.teamId}/databases/${this.databaseId}/query`,
-			encodeURIComponent(query),
+			query,
 			{
 				headers: {
 					"Content-Type": "application/json",
